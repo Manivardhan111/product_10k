2b24d5f289e59ad64484660d76c094159860fb86
manivardhan111 <tejomanivardhan.jakka@gmail.com>
2023-12-23 19:22:48 +0530

    changes

--- Apps/Mentor/views.py
+++ Apps/Mentor/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-
+ 
 # Create your views here.
 @csrf_exempt
 def mentor_sample(request):
