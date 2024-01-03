5aed5ee4a3a9f5972fef859e232aedf3645e5949
manivardhan111 <tejomanivardhan.jakka@gmail.com>
2024-01-03 22:47:14 +0530

    deleted al files

--- Apps/SalesPerson/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import admin_salesPerson
-
-class RegisterSalesPersonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=admin_salesPerson
-        fields="__all__"
